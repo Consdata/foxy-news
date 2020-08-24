b6d597e4fd103c5fcd7b019af2ffc2ecade7242f
Grzegorz Lipecki <glipecki@consdata.pl>
2020-08-24 09:36:44 +0200

    fixes add link modal

--- apps/functions/src/app/chatbot/chatbot-shortcut-add-link-tech.ts
+++ apps/functions/src/app/chatbot/chatbot-shortcut-add-link-tech.ts
@@ -3,25 +3,25 @@ import {newsletterModalView} from '../newsletter-modal-view';
 import {ChatbotInteraction} from './chatbot-interaction';
 
 export async function onShortcutAddLinkTech(slackHttpHeaders: any, interactionRequest: ChatbotInteraction) {
-    await nodeFetch(`https://slack.com/api/views.open`, {
-        method: 'POST',
-        headers: slackHttpHeaders,
-        body: JSON.stringify({
-            trigger_id: interactionRequest.trigger_id,
-            view: newsletterModalView(
-                'addLink_tech', 
-                [
-                    {key: 'devops', label: 'DevOps'},
-                    {key: 'frontend', label: 'Frontend'},
-                    {key: 'webdev', label: 'WebDev'},
-                    {key: 'cloud', label: 'Cloud'},
-                    {key: 'crafsmanship', label: 'Crsafsmanship'},
-                    {key: 'uiux', label: 'Ui/Ux'},
-                    {key: 'linux', label: 'Linux'},
-                    {key: 'security', label: 'Security'},
-                    {key: 'misc', label: 'Miscellaneous'},
-                ]
-            )
-        })
+  await nodeFetch(`https://slack.com/api/views.open`, {
+    method: 'POST',
+    headers: slackHttpHeaders,
+    body: JSON.stringify({
+      trigger_id: interactionRequest.trigger_id,
+      view: newsletterModalView(
+        'addLink_tech',
+        [
+          {key: 'devops', label: 'DevOps'},
+          {key: 'frontend', label: 'Frontend'},
+          {key: 'webdev', label: 'WebDev'},
+          {key: 'cloud', label: 'Cloud'},
+          {key: 'crafsmanship', label: 'Crsafsmanship'},
+          {key: 'uiux', label: 'Ui/Ux'},
+          {key: 'linux', label: 'Linux'},
+          {key: 'security', label: 'Security'},
+          {key: 'misc', label: 'Miscellaneous'},
+        ]
+      )
     })
+  });
 }
